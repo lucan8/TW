fe14dcd11fb7a97dcb1e1b938bc7766aa8986942
lucan8 <gamer.lucan72@gmail.com>
2024-04-03 08:23:52 +0300

    Added to github

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from cs50 import SQL
+app = Flask(__name__)
+
+db = SQL("sqlite:///tasks.db")
+
+@app.route("/")
+def index():
+    user_info = db.execute("SELECT name, title, XP, LVL FROM user")
+    
+    return render_template("index.html", name = user_info[0]['name'], title = user_info[0]['title'], XP = user_info[0]['XP'], LVL = user_info[0]['LVL'])
