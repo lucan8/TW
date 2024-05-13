31c0ff5714fc2b38508ad6ab86b49d9542455f4d
lucan8 <gamer.lucan72@gmail.com>
2024-05-13 17:54:02 +0300

    fieldsets completed

--- lab9/form.js
+++ lab9/form.js
@@ -1,6 +1,14 @@
 
 window.onload = function(){
-  
+  for (elem of document.getElementsByTagName("option"))
+    elem.value =  elem.innerHTML;
+
+  social_creds = document.getElementById('creds');
+  range_creds = document.getElementById('range_creds');
+
+  range_creds.onchange = function(event){
+    social_creds.innerHTML = event.target.value
+  }
   /* Adăugați cod pentru schimbarea culorii de fundal și 
      pentru eticheta cu valoarea creditului social      */
      
