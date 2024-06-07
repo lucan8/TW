a428a6a3f23ccf3ac45537fe22519fd4d504f880
lucan8 <gamer.lucan72@gmail.com>
2024-06-07 21:22:59 +0300

    automatically created sql database and tables

--- project/app.js
+++ project/app.js
@@ -8,7 +8,8 @@ const session = require("express-session");
 const mySQLStore = require('express-mysql-session')(session);
 const path = require('path');
 
-//Tabele vor fii create automat cand rulati codul
+const app = express();
+
 const sql_options = {
     host:'localhost',
     user:'root',
@@ -45,238 +46,280 @@ const session_store_options = {
     }
 };
 
-const pool = mysql.createPool(sql_options).promise();
+//Connecting to database
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'1234'
+});
 
-const app = express();
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({extended : false}))
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'public/templates'));
+var pool;
 
 async function myQuery(q, wildcards){
     console.log(q, wildcards);
     return (await pool.query(q, wildcards))[0];
 }
 
-//Creating the tables if needed
-//MAKE THIS FUNCTION
-myQuery(`CREATE DATABASE IF NOT EXISTS task_hero;`);
-myQuery(`CREATE TABLE IF NOT EXISTS users(
-        user_id int PRIMARY KEY NOT NULL,
-        username varchar(100) NOT NULL,
-        password varchar(255) NOT NULL,
-        email varchar(255) NOT NULL UNIQUE,
-        xp int NOT NULL DEFAULT 0);`
-       );
-myQuery(`CREATE TABLE IF NOT EXISTS daily_quests(
-        user_id	int NOT NULL,
-        d_quest_id int NOT NULL,
-        quest_name varchar(50) NOT NULL UNIQUE,        
-        quest_details varchar(100),
-        PRIMARY KEY(user_id, d_quest_id),
-        FOREIGN KEY(user_id) REFERENCES users(user_id)
-        );`
-       );
-       
-myQuery(`CREATE TABLE IF NOT EXISTS daily_quest_completion(
-        user_id	int NOT NULL,
-        date date NOT NULL,
-        nr_completed int NOT NULL DEFAULT 0,
-        nr_tasks int NOT NULL,
-        PRIMARY KEY(user_id, date),
-        FOREIGN KEY(user_id) REFERENCES users(user_id)
-        );`
-      )
 
-const sessionStore = new mySQLStore(session_store_options, pool);
-const session_options = {
-    secret:'lorem sus',
-    store: sessionStore,
-    saveUninitialized: false,
-    resave: false,
-    cookie:{maxAge : 30 * 24 * 60 * 60 * 1000}
-};
-app.use(session(session_options));
-
-const isAuth = (req, res, next) =>{
-    if (req.session.user_id)
-        next();
+db.connect(async (e)=>{
+    if (e)
+        console.log(e);
     else
-        res.redirect('/login');
-}
-async function getUser(email){
-    return (await myQuery("SELECT * FROM users WHERE email = ?", [email]))[0];
-}
-
-//Returns true if user is succesfully created, false otherwise
-async function createUser(name, email, password){
-    const hashed_pass = await bcrypt.hash(password, 10);
-    return await myQuery("INSERT INTO users(user_id, username, email, password) VALUES(?, ?, ?, ?)",
-                [await getMaxUserId() + 1, name, email, hashed_pass]);
-}
-
-async function removeUser(user_id){
-    await myQuery("DELETE FROM users WHERE user_id = ?",
-                  [user_id])[0];
-}
-
-async function getMaxUserId(){
-    const max_id = (await myQuery("SELECT MAX(user_id) AS max_id FROM users"))[0].max_id;
-
-    if (max_id)
-        return max_id;
-    return 0;
-}
-
-async function getQuest(user_id, quest_name){
-    return (await myQuery("SELECT * FROM daily_quests WHERE user_id = ? AND quest_name = ?",
-                          [user_id, quest_name]))[0];
-}
-
-async function getAllQuests(user_id){
-    let quests_aux = (await myQuery("SELECT quest_name FROM daily_quests WHERE user_id = ?",
-                      [user_id]));
-    let quests = [];
-    for (let q of quests_aux)
-        quests.push(q.quest_name);
-
-    return quests;
-}
-async function createQuest(user_id, quest_name){
-    await myQuery("INSERT INTO daily_quests(user_id, d_quest_id, quest_name) VALUES(?,?,?)",
-                  [user_id, await getMaxQuestId(user_id) + 1, quest_name])[0];
-}
-
-async function removeQuest(user_id, quest_name){
-    await myQuery("DELETE FROM daily_quests WHERE user_id = ? AND quest_name = ?",
-                  [user_id, quest_name])[0];
-}
-
-async function getMaxQuestId(user_id){
-    const max_id = (await myQuery("SELECT MAX(d_quest_id) AS max_id FROM daily_quests WHERE user_id = ?",
-                    [user_id]))[0].max_id;
-    console.log(max_id);
-
-    if (max_id)
-        return max_id;
-    return 0;
-}
-
-app.listen(3000, (err)=>{
-    if (err)
-        console.log(err);
-    console.log("Listening on port 3000");
-});
-
-app.get('/', isAuth, (req, res) => {
-    res.render('index');
-});
-
-app.get('/about_us', (req, res) => {
-    res.render('about_us');
-});
-
-
-//MAKE USER AUTH ROUTER
-app.get('/login', (req, res) => {
-    res.render('login', {email: '', password: '', err_msg : ''});
-});
-
-
-app.post('/login', async (req, res) => {
-    const {email, password} = req.body;
-    try{
-        const user = await getUser(email);
-        //No matching email, sending err msg
-        if (!user)
-            res.render('login', {email: email, password: password, err_msg : 'Incorrect email'});
-        //Password does not match, sending err msg
-        else if (!(await bcrypt.compare(password, user.password)))
-            res.render('login', {email: email, password: password, err_msg : 'Incorrect password'});
-
-        else {//Creating session for user and redirecting to home page
-            req.session.user_id = user.user_id;
-            res.redirect('/');
-        }
-    } catch(e){
-        console.log(e);
-        res.sendStatus(500);
-    }
-});
-
-app.get('/register', (req, res) => {
-    res.render('register');
-});
-
-app.post('/register', async (req, res) => {
-    const {username, email, password} = req.body;
-    try{
-        createUser(username, email, password)
-        .then(()=>res.redirect('/login'));
-    }
-    catch(e){
-        console.log(e);
-        res.sendStatus(500);
-    }
-});
-
-app.post('/register/verif_email', async (req, res) =>{
-    const {email} = req.body;
-    try{
-        const user = await getUser(email);
-        if (user)
-            res.send('Email in use!');
+        console.log('Connected to database server');
+    //Creating the database if needed
+    db.query(`CREATE DATABASE IF NOT EXISTS task_hero`, async (e) =>{
+        if (e)
+            console.log(e);
         else
-            res.send(200);
-    } catch(e){
-        console.log(e);
-        res.sendStatus(500)
-    }
-})
+            console.log('Created database task_hero!');
 
-app.get('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        console.log(err);
-    });
-    res.redirect('/login');
-});
+        //Creating a connection pool and all the tables needed
+        pool = mysql.createPool(sql_options).promise();   
+        createTableUsers().then(createTableDailyQuests).then(createTableDailyQuestsComp);
 
-//MAKE DAILY_QUESTS ROUTER
-app.get('/daily_quests', isAuth, async(req, res) => {
-    res.render('daily_quests', {quests: await getAllQuests(req.session.user_id)});
-});
+        app.use(express.static('public'))
+        app.use(express.json())
+        app.use(express.urlencoded({extended : false}))
 
-app.post('/daily_quests/add', isAuth, async (req, res) =>{
-    const user_id = req.session.user_id;
-    const quest_name = req.body.quest_name;
-    try{
-        if (await getQuest(user_id, quest_name))
-            res.send('Task already exists!');
-        else{
-            await createQuest(user_id, quest_name);
-            res.sendStatus(200);
+
+        app.set('view engine', 'ejs')
+        app.set('views', path.join(__dirname, 'public/templates'));
+
+
+        var sessionStore = new mySQLStore(session_store_options, pool);
+        const session_options = {
+            secret:'lorem sus',
+            store: sessionStore,
+            saveUninitialized: false,
+            resave: false,
+            cookie:{maxAge : 30 * 24 * 60 * 60 * 1000}
+        };
+        app.use(session(session_options));
+
+        const isAuth = (req, res, next) =>{
+            if (req.session.user_id)
+                next();
+            else
+                res.redirect('/login');
         }
-    } catch(e){
-        console.log(e);
-        res.sendStatus(500);
-    }
-})
-app.post('/daily_quests/remove', isAuth, async (req, res) =>{
-    const user_id = req.session.user_id;
-    const quest_name = req.body.quest_name;
-    try{
-        await removeQuest(user_id, quest_name);
-        res.sendStatus(200);
-    } catch(e){
-        console.log(e);
-        res.sendStatus(500);
-    }
-})
-//MAKE TASKS ROUTER
-app.get('/tasks', isAuth, (req, res) => {
-    res.render('tasks');
+        async function getUserByEmail(email){
+            return (await myQuery("SELECT * FROM users WHERE email = ?", [email]))[0];
+        }
+
+        async function getUserById(id){
+            return (await myQuery("SELECT * FROM users WHERE user_id = ?", [id]))[0];
+        }
+
+        //Returns true if user is succesfully created, false otherwise
+        async function createUser(name, email, password){
+            const hashed_pass = await bcrypt.hash(password, 10);
+            return await myQuery("INSERT INTO users(user_id, username, email, password) VALUES(?, ?, ?, ?)",
+                        [await getMaxUserId() + 1, name, email, hashed_pass]);
+        }
+
+        async function removeUser(user_id){
+            await myQuery("DELETE FROM users WHERE user_id = ?",
+                        [user_id])[0];
+        }
+
+        async function getMaxUserId(){
+            const max_id = (await myQuery("SELECT MAX(user_id) AS max_id FROM users"))[0].max_id;
+
+            if (max_id)
+                return max_id;
+            return 0;
+        }
+
+        async function getQuest(user_id, quest_name){
+            return (await myQuery("SELECT * FROM daily_quests WHERE user_id = ? AND quest_name = ?",
+                                [user_id, quest_name]))[0];
+        }
+
+        async function getAllQuests(user_id){
+            let quests_aux = (await myQuery("SELECT quest_name FROM daily_quests WHERE user_id = ?",
+                            [user_id]));
+            let quests = [];
+            for (let q of quests_aux)
+                quests.push(q.quest_name);
+
+            return quests;
+        }
+        async function createQuest(user_id, quest_name){
+            await myQuery("INSERT INTO daily_quests(user_id, d_quest_id, quest_name) VALUES(?,?,?)",
+                        [user_id, await getMaxQuestId(user_id) + 1, quest_name])[0];
+        }
+
+        async function removeQuest(user_id, quest_name){
+            await myQuery("DELETE FROM daily_quests WHERE user_id = ? AND quest_name = ?",
+                        [user_id, quest_name])[0];
+        }
+
+        async function getMaxQuestId(user_id){
+            const max_id = (await myQuery("SELECT MAX(d_quest_id) AS max_id FROM daily_quests WHERE user_id = ?",
+                            [user_id]))[0].max_id;
+            console.log(max_id);
+
+            if (max_id)
+                return max_id;
+            return 0;
+        }
+
+        app.listen(3000, (err)=>{
+            if (err)
+                console.log(err);
+            else
+                console.log("Listening on port 3000");
+        });
+
+        app.get('/', isAuth, async (req, res) => {
+            const user = await getUserById(req.session.user_id);
+            res.render('index', {username: user.username, xp: user.xp});
+        });
+
+        app.get('/about_us', (req, res) => {
+            res.render('about_us');
+        });
+
+
+        //MAKE USER AUTH ROUTER
+        app.get('/login', (req, res) => {
+            res.render('login', {email: '', password: '', err_msg : ''});
+        });
+
+
+        app.post('/login', async (req, res) => {
+            const {email, password} = req.body;
+            try{
+                const user = await getUserByEmail(email);
+                //No matching email, sending err msg
+                if (!user)
+                    res.render('login', {email: email, password: password, err_msg : 'Incorrect email'});
+                //Password does not match, sending err msg
+                else if (!(await bcrypt.compare(password, user.password)))
+                    res.render('login', {email: email, password: password, err_msg : 'Incorrect password'});
+
+                else {//Creating session for user and redirecting to home page
+                    req.session.user_id = user.user_id;
+                    res.redirect('/');
+                }
+            } catch(e){
+                console.log(e);
+                res.sendStatus(500);
+            }
+        });
+
+        app.get('/register', (req, res) => {
+            res.render('register');
+        });
+
+        app.post('/register', async (req, res) => {
+            const {username, email, password} = req.body;
+            try{
+                createUser(username, email, password)
+                .then(()=>res.redirect('/login'));
+            }
+            catch(e){
+                console.log(e);
+                res.sendStatus(500);
+            }
+        });
+
+        app.post('/register/verif_email', async (req, res) =>{
+            const {email} = req.body;
+            try{
+                const user = await getUserByEmail(email);
+                if (user)
+                    res.send('Email in use!');
+                else
+                    res.sendStatus(200);
+            } catch(e){
+                console.log(e);
+                res.sendStatus(500)
+            }
+        })
+
+        app.get('/logout', (req, res) => {
+            req.session.destroy((err) => {
+                console.log(err);
+            });
+            res.redirect('/login');
+        });
+
+        //MAKE DAILY_QUESTS ROUTER
+        app.get('/daily_quests', isAuth, async(req, res) => {
+            const user = await getUserById(req.session.user_id);
+            res.render('daily_quests', {quests: await getAllQuests(user.user_id), 
+                                        username: user.username, xp: user.xp});
+        });
+
+        app.post('/daily_quests/add', isAuth, async (req, res) =>{
+            const user_id = req.session.user_id;
+            const quest_name = req.body.quest_name;
+            try{
+                if (await getQuest(user_id, quest_name))
+                    res.send('Task already exists!');
+                else{
+                    await createQuest(user_id, quest_name);
+                    res.sendStatus(200);
+                }
+            } catch(e){
+                console.log(e);
+                res.sendStatus(500);
+            }
+        })
+        app.post('/daily_quests/remove', isAuth, async (req, res) =>{
+            const user_id = req.session.user_id;
+            const quest_name = req.body.quest_name;
+            try{
+                await removeQuest(user_id, quest_name);
+                res.sendStatus(200);
+            } catch(e){
+                console.log(e);
+                res.sendStatus(500);
+            }
+        })
+        //MAKE TASKS ROUTER
+        app.get('/tasks', isAuth, async (req, res) => {
+            const user = await getUserById(req.session.user_id);
+            res.render('tasks', {username: user.username, xp: user.xp});
+        });
+
+        app.get('*', function(req, res){
+            res.sendFile(path.join(__dirname, 'public/templates/404.html'));
+        });
+
+    });
 });
+async function createTableUsers(){
+    myQuery(`CREATE TABLE IF NOT EXISTS users(
+            user_id int PRIMARY KEY NOT NULL,
+            username varchar(100) NOT NULL,
+            password varchar(255) NOT NULL,
+            email varchar(255) NOT NULL UNIQUE,
+            xp int NOT NULL DEFAULT 0);`
+          );
+}
 
-
+async function createTableDailyQuests(){
+    myQuery(`CREATE TABLE IF NOT EXISTS daily_quests(
+            user_id	int NOT NULL,
+            d_quest_id int NOT NULL,
+            quest_name varchar(50) NOT NULL UNIQUE,        
+            quest_details varchar(100),
+            PRIMARY KEY(user_id, d_quest_id),
+            FOREIGN KEY(user_id) REFERENCES users(user_id)
+            );`
+          );
+}
+       
+async function createTableDailyQuestsComp(){
+    myQuery(`CREATE TABLE IF NOT EXISTS daily_quest_completion(
+            user_id	int NOT NULL,
+            date date NOT NULL,
+            nr_completed int NOT NULL DEFAULT 0,
+            nr_tasks int NOT NULL,
+            PRIMARY KEY(user_id, date),
+            FOREIGN KEY(user_id) REFERENCES users(user_id)
+            );`
+          );
+}
